8034dd62f840da0611ae72461339d156b1f8cd06
who-sane <103615588@student.swin.edu.au>
2021-07-19 10:16:36 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-cruzs5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-cruzs5)
---
+++ index.ts
@@ -0,0 +1,38 @@
+// Import stylesheets
+//import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+// var test: string = "yes";
+// var testnum: number = 19.9
+// var hi: boolean = false;
+// var any: any = "word";
+
+// any = 8;
+
+// const constant = true;
+// const color = "Brown";
+// brown cant equal anything else
+
+// let year: number = 7;
+
+button.addEventListener('click', event =>); {
+  button.addEventListener('click', fizzbuzz);
+// here is where u put a commmand
+//}
+
+function fizzbuzz() {
+  for (let i = 1; i <= 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 == 0) {
+      console.log('Fizz');
+    } else if (i % 5 == 0) {
+      console.log('Buzz');
+    }
+  }
+}
+
+}
